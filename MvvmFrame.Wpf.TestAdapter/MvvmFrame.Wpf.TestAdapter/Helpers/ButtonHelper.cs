b6ba40c29c0769341a1fc299eb7c168e59a3641f
Zhuel Itua <ituazhuel@hotmail.com>
2019-10-08 18:30:41 +0300

    (dev-1) added Click for button

---
+++ MvvmFrame.Wpf.TestAdapter/MvvmFrame.Wpf.TestAdapter/Helpers/ButtonHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace MvvmFrame.Wpf.TestAdapter.Helpers
+{
+    /// <summary>
+    /// Helper for <see cref="Button"/>
+    /// </summary>
+    public static class ButtonHelper
+    {
+        /// <summary>
+        /// Click button
+        /// </summary>
+        /// <param name="button"></param>
+        /// <param name="debugInfo"></param>
+        public static void Click(this Button button, string debugInfo = null)
+        {
+            button.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+
+            if (debugInfo != null)
+                Debug.WriteLine(debugInfo);
+            else
+                Debug.WriteLine($"Click button {button.Name}");
+        }
+    }
+}
