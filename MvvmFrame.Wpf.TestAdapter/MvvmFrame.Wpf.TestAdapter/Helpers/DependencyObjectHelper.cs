1df2b6b474343a62ff4431e927ea4565e97d9da1
Getcu Reone <GetcuReone@hotmail.com>
2019-10-24 21:12:02 +0300

    (dev-1) added FindParentByType method

---
+++ MvvmFrame.Wpf.TestAdapter/MvvmFrame.Wpf.TestAdapter/Helpers/DependencyObjectHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MvvmFrame.Wpf.TestAdapter.Helpers
+{
+    /// <summary>
+    /// <see cref="DependencyObject"/> helper
+    /// </summary>
+    public static class DependencyObjectHelper
+    {
+        /// <summary>
+        /// Find parent
+        /// </summary>
+        /// <typeparam name="TDependencyObject">type of object sought</typeparam>
+        /// <param name="child"></param>
+        /// <returns>parent object</returns>
+        /// <remarks>
+        /// Thanks https://stackoverflow.com/questions/15198104/find-parent-of-control-by-name
+        /// </remarks>
+        public static TDependencyObject FindParentByType<TDependencyObject>(this DependencyObject child)
+            where TDependencyObject : DependencyObject
+        {
+            //get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            //we've reached the end of the tree
+            if (parentObject == null) return null;
+
+            //check if the parent matches the type we're looking for
+            TDependencyObject parent = parentObject as TDependencyObject;
+            if (parent != null)
+                return parent;
+            else
+                return FindParentByType<TDependencyObject>(parentObject);
+        }
+
+    }
+}
