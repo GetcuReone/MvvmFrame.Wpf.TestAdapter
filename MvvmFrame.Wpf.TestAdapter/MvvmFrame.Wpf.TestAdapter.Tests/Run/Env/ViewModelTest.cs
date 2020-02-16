e30b71ab9dc9e22bc8ee23162a95d6849a0de6a5
GetcuReone <getcureone@hotmail.com>
2020-02-16 13:53:14 +0300

    restore RunTests

---
+++ MvvmFrame.Wpf.TestAdapter/MvvmFrame.Wpf.TestAdapter.Tests/Run/Env/ViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using GetcuReone.MvvmFrame.Wpf;
+using GetcuReone.MvvmFrame.Wpf.Commands;
+using GetcuReone.MvvmFrame.Wpf.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmFrame.Wpf.TestAdapter.Tests.Run.Env
+{
+    public sealed class ViewModelTest : ViewModelBase
+    {
+        public Command Command { get; set; }
+
+        protected override void Initialize()
+        {
+
+        }
+
+        protected override ValueTask OnGoPageAsync(object navigateParam) => default;
+
+        protected override ValueTask OnLeavePageAsync(NavigatingEventArgs args) => default;
+
+        protected override ValueTask OnLoadPageAsync() => default;
+    }
+}
