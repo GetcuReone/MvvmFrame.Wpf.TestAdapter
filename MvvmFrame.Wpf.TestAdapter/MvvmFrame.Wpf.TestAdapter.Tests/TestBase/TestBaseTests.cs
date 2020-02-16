0b82d11e5708efb9ea38a43362998cfc1fa83a10
GetcuReone <getcureone@hotmail.com>
2020-02-16 14:44:57 +0300

    added test ErrorReturnCheckTestCase

---
+++ MvvmFrame.Wpf.TestAdapter/MvvmFrame.Wpf.TestAdapter.Tests/TestBase/TestBaseTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MvvmFrame.Wpf.TestAdapter.Tests.TestBase
+{
+    [TestClass]
+    public sealed class TestBaseTests : GetcuReone.MvvmFrame.Wpf.TestAdapter.TestBase
+    {
+        [Timeout(Timeouts.HundredMillisecodnds)]
+        [TestMethod]
+        [Description("[test_base] error return check")]
+        public void ErrorReturnCheckTestCase()
+        {
+            string message = "is error";
+
+            var error = ExpectedException<Exception>(() => throw new Exception(message));
+
+            Assert.AreEqual(message, error.Message, "Message text does not match");
+        }
+    }
+}
---
+++ MvvmFrame.Wpf.TestAdapter/MvvmFrame.Wpf.TestAdapter/TestBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GetcuReone.MvvmFrame.Wpf.TestAdapter
+{
+    /// <summary>
+    /// Class base for test
+    /// </summary>
+    public abstract class TestBase
+    {
+        /// <summary>
+        /// Method expects error from <paramref name="action"/>
+        /// </summary>
+        /// <typeparam name="TException">type exception</typeparam>
+        /// <param name="action">action</param>
+        /// <returns></returns>
+        protected virtual TException ExpectedException<TException>(Action action)
+            where TException : Exception
+        {
+            try
+            {
+                action();
+            }
+            catch (TException e)
+            {
+                return e;
+            }
+            catch
+            {
+                throw;
+            }
+
+            Assert.Fail("The method worked without errors");
+            return default;
+        }
+    }
+}
