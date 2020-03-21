e3182f6f49571ec5987ca4bc4812953c863c4c98
GetcuReone <getcureone@hotmail.com>
2020-03-21 16:14:49 +0300

    Translated tests to the public library

---
+++ MvvmFrame.Wpf.TestAdapter/Infrastructure/TestCommon/TC.cs
@@ -0,0 +1,11 @@
+﻿namespace TestCommon
+{
+    public static class TC
+    {
+        public static class Projects
+        {
+            public const string Infrastructure = "Infra";
+            public const string GwtTestAdapter = "GwtTestAdapter";
+        }
+    }
+}
