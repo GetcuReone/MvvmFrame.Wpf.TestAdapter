2bb057d6d27dc47d378d5a19fa904a01b398f5c6
Juel <GetcuReone@hotmail.com>
2020-06-24 21:12:49 +0300

    added IsPageTypeTests

---
+++ MvvmFrame.Wpf.TestAdapter/MvvmFrame.Wpf.TestAdapter.Tests/FrameTest/Env/ViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using GetcuReone.MvvmFrame.Wpf;
+using GetcuReone.MvvmFrame.Wpf.EventArgs;
+
+namespace MvvmFrame.Wpf.TestAdapter.Tests.FrameTest.Env
+{
+    public sealed class ViewModelTest : ViewModelBase
+    {
+        protected override void Initialize()
+        {
+
+        }
+
+        protected override ValueTask OnGoPageAsync(object navigateParam) => default;
+
+        protected override ValueTask OnLeavePageAsync(NavigatingEventArgs args) => default;
+
+        protected override ValueTask OnLoadPageAsync() => default;
+    }
+}
